64d3436c6bc0eb8a1bc2112943eb5b8df81e6766
userain <userrain@163.com>
2015-07-02 16:11:21 +0800

    Update RServer_threadsPoll.h

--- RServer_threadsPoll.h
+++ RServer_threadsPoll.h
@@ -7,11 +7,11 @@ using namespace std;
 #define _RServer_threadsPoll_H
 class RServer_threadsPoll{
 private:
-    bool pollStartStatus;  
-    list<pthread_t> threadsList;
-    pthread_mutex_t threadsPollLock;
-    RServer_threadTaskQueue *taskQueue;
-    pthread_cond_t  queueReadyCond;
+    bool pollStartStatus;                   //线程池的启动状态
+    list<pthread_t> threadsList;            //线程池中的每一个线程描述符
+    pthread_mutex_t threadsPollLock;        //线程池中用于控制信号量访问的锁
+    RServer_threadTaskQueue *taskQueue;     //线程池的中线程的任务队列
+    pthread_cond_t  queueReadyCond;         //线程池中的任务到来信号量
 public:
     RServer_threadsPoll(unsigned int nums,RServer_threadTaskQueue* queue,void*(*routine)(void *));
     int lock();
